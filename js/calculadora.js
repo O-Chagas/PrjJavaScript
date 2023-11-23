474d457d49001991f315d39dd477aac1bc79f882
CAUA SILVA CHAGAS <caua.schagas@senacsp.edu.br>
2023-11-22 22:23:33 -0300

    Var/Const/Let/Function

--- js/calculadora.js
+++ js/calculadora.js
@@ -5,30 +5,36 @@
 //4 Mostrar o resultado
 let resp;
 let num1 = parseFloat(prompt('Digite o primeiro número:'));
-let num2 = parseFloat(prompt('Digite o segundo número:'));
 let operacao = prompt('Digite a operação que deseja realizar: [+] - [-] - [x] - [/]');
+let num2 = parseFloat(prompt('Digite o segundo número:'));
 
 if (isNaN(num1) == true || isNaN(num2) == true) {
     alert('Operação inválida');
 }
-else {
-
-
-    if (operacao == '+') {
-        resp = num1 + num2;
-    }
-
-    else if (operacao == '-') {
-        resp = num1 - num2;
-    }
-
-    else if (operacao == 'x') {
-        resp = num1 * num2;
-    }
-
-    else if (operacao == '/') {
-        resp = num1 / num2;
-    }
+else if (operacao == '+') {
+    resp = num1 + num2;
 }
 
-document.write('<p class = "resultado"> O resultado da operação é: </p>', resp);
+else if (operacao == '-') {
+    resp = num1 - num2;
+}
+
+else if (operacao == 'x') {
+    resp = num1 * num2;
+}
+
+else if (operacao == '/') {
+    resp = num1 / num2;
+}
+
+else {
+    resp = 'Operação Inválida!';
+}
+
+if (isNaN(resp)) {
+    document.write('A operação é inválida, tente novamente!');
+}
+
+else {
+    document.write('O resultado da operação é: ', resp);
+}
---
+++ js/letVarConst.js
@@ -0,0 +1,8 @@
+var numero1 = 8;
+
+function exibeValor(){
+    var numero1 = 9;
+    document.write('O valor do número é: ', numero1);
+}
+
+exibeValor();
