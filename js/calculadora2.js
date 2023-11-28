4d3638a5785bf49b0dc2442471d96d662ddd968d
CAUA SILVA CHAGAS <caua.schagas@senacsp.edu.br>
2023-11-27 21:57:50 -0300

    Finalizando calculadora

--- js/calculadora2.js
+++ js/calculadora2.js
@@ -1,6 +1,31 @@
+
+//pegando o valor da caixa de texto via querySelector
+console.log(document.querySelector('#numero1').value);
+
+//pegando o valor da caixa de texto via getElementById
+console.log(document.getElementById('numero1').value);
+
+//pegando o valor da caixa de texto via getElementByTagName
+//IMPORTANTE getElementByTagName retorna um array de tags
+console.log(document.getElementsByTagName('input')[0].value);
+
+//pegando o valor da caixa de texto via getElementByClassName
+//IMPORTANTE getElementByClassName retorna um array de elementos
+//com a referida classe
+console.log(document.getElementsByClassName('form-input')[0].value);
+
+//alterando valores de uma tag utilizando o innerHTML
+//console.log(document.getElementsByTagName('label')[0].innerHTML='Meu número');
+
+//capturando valores de uma tag utilizando o innerHTML
+console.log(document.getElementsByTagName('label')[0].innerHTML);
+
+
+
+
 function calcular() {
-    const primeiroNumero = document.formCalculadora.num1.value;
-    const segundoNumero = document.formCalculadora.num2.value;
+    const primeiroNumero = parseFloat(document.formCalculadora.num1.value);
+    const segundoNumero = parseFloat(document.formCalculadora.num2.value);
     const operacao = document.formCalculadora.oper.value;
     let resultado;
 
@@ -17,12 +42,20 @@ function calcular() {
         resultado = primeiroNumero * segundoNumero;
     }
 
+    /*
     //janela
-    alert('O resultado da operação é: ', resultado);
+    alert('O resultado da operação é: ' + resultado);
 
     //console log
     console.log(('O resultado da operação é: ', resultado));
 
     //documento
     document.write('O resultado da operação é: ', resultado);
+    */
+
+    const caixaResultado = document.getElementById('resultado');
+    caixaResultado.value = resultado;
+
+    const paragrafoResultado = document.getElementById('pResultado');
+    paragrafoResultado.innerHTML = resultado;
 }
